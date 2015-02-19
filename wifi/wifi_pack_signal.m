5e61dad50be212ab34af223a56e155b25741ee2d
RAKESH MISRA <rakeshmisra@stanford.edu>
2015-02-19 03:13:27 -0800

    added rate_chart as an argument

--- wifi/wifi_pack_signal.m
+++ wifi/wifi_pack_signal.m
@@ -1,11 +1,11 @@
 
-function tx_sig_field = wifi_pack_signal(rate, len)
+function tx_sig_field = wifi_pack_signal(rate, rate_chart, len)
   %display('wifi_parse_signal input:');
   %rx_sig_field
 
   rate_idx = [6 7 2 3 4 5 0 1];
 
-  rate_chart = [6 9 12 18 24 36 48 54];
+  %rate_chart = [6 9 12 18 24 36 48 54];
   %modu_chart = [1 1 2 2 4 4 6 6];
   %code_chart = [60 90 60 90 60 90 80 90];
   %ndbps_chart = [24 36 48 72 96 144 192 216];
@@ -17,7 +17,7 @@ function tx_sig_field = wifi_pack_signal(rate, len)
   rate_field = zeros(1,4);
   rate_field(4) = 1;
 
-  idx = find(rate == rate_chart);
+  idx = find(rate_chart == rate);
   rate_idx = rate_idx(idx);
   rate_field(1:3) = de2bi(rate_idx, 3, 'left-msb');
 
