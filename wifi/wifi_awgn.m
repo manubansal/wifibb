52fc7bc2b0d767341c58499a219f899c562fbce6
Manu Bansal <manub@stanford.edu>
2015-06-24 18:27:25 -0700

    added seeds for reproducible channel and noise

--- wifi/wifi_awgn.m
+++ wifi/wifi_awgn.m
@@ -1,16 +1,25 @@
-function noisy_samples = wifi_awgn(clean_samples, snr, sigpower_dBW)
-  randstreamseed = 5489;		%a fixed value for reproducible results
+function noisy_samples = wifi_awgn(clean_samples, snr, sigpower_dBW,seed)
+  if nargin < 4
+    randstreamseed = 5489;		%a fixed value for reproducible results
+  else
+    randstreamseed = seed;
+  end
   %randstreamseed = 'shuffle';
   if snr < Inf
     if strcmp(version('-release'),'2010b')
-      randn('seed',1);
+      randn('seed',randstreamseed);
+      if nargin < 3
+      noisy_samples = awgn(clean_samples, snr, 'measured');
+      else
+      noisy_samples = awgn(clean_samples, snr, sigpower_dBW);
+      end
     else		%assuming higher than 2010b
       randstream = RandStream('mt19937ar','seed',randstreamseed);
-    end
-    if nargin < 3
-    noisy_samples = awgn(clean_samples, snr, 'measured');
-    else
-    noisy_samples = awgn(clean_samples, snr, sigpower_dBW);
+      if nargin < 3
+      noisy_samples = awgn(clean_samples, snr, 'measured',randstream);
+      else
+      noisy_samples = awgn(clean_samples, snr, sigpower_dBW,randstream);
+      end
     end
   else
     noisy_samples = clean_samples;
