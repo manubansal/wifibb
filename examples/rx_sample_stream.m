6761ae73f1bfd28c47c8dcf410e17f33276ee227
Aditya Gudipati <adityagudipati@gmail.com>
2016-01-18 17:49:52 -0800

    printing out number of correctly decoded packets

--- examples/rx_sample_stream.m
+++ examples/rx_sample_stream.m
@@ -11,3 +11,10 @@ cplen = common_params.cplen;
 
 [rx_pkts, pkt_start_points] = wifi_rx_pkt_train(sim_params, ...
     common_params, rx_params, samples, tag, cplen);
+
+crc_vec = zeros(1, length(rx_pkts));
+for idx = 1:length(rx_pkts)
+    crc_vec(idx) = rx_pkts{idx}{3};
+end
+display(['Packets received :', num2str(length(rx_pkts))])
+display(['CRC valid :', num2str(length(find(crc_vec == 1)))])
